bd5c087add0c6a25bac5991f35f61930e62f52c8
Archadox <mintysudo@gmail.com>
2020-11-18 19:45:46 -0500

    FINALLY got the text advancement for the intro working. Keyboard is a terrible choice of module though because now the entire thing has to be run as root

--- Useful_Stuff/Dungeon_Crawler_2/Game_Files/Crawler2.py
+++ Useful_Stuff/Dungeon_Crawler_2/Game_Files/Crawler2.py
@@ -1,4 +1,37 @@
-import os, sys
+import os, time
+import keyboard #A terrible choice in module because now the entire progrm has to be run as root
+#Not good enough yet to try the alternatives     
+
+def original_intro():
+    #Probably doesn't need to be a function but whatever
+    #Designed to be executed by the charcre function after a successful character creation
+    upperino = 1
+    while True:
+        if keyboard.is_pressed('space'):
+            if upperino == 1:
+                print('Your grave stirrs as you awaken from your near eternal slumber\n')
+                upperino = 2
+                time.sleep(.1)
+
+            elif upperino == 2:
+                print('You rise with a rusted sword in your hand and a key on your chest\n')
+                upperino = 3
+                time.sleep(.1)
+
+            elif upperino == 3:
+                print('Bla bla bla, you need to get powers to save an ancient kingdom\n')
+                upperino = 4
+                time.sleep(.1)
+
+            elif upperino == 4:
+                print('You enter a nearby village and purchase clothes with the ancient gold in your coffin\n')
+                upperino = 5
+                time.sleep(.1)
+
+            elif upperino == 5:
+                upperino = None
+                #time.sleep(.1)
+                break
 
 def intro():
     #The intro that plays whenever the game is begun
@@ -16,6 +49,12 @@ def namecheck():
     except FileNotFoundError:
         charcre()
 
+def namesave():
+    #Saves the name of the current player in a temporary file
+    namefileer = open('../Temporary_Files/Current_Player_Name', 'w+')
+    namefileer.write(name)
+    namefileer.close()
+
 def charcre():
     #A shittily named function to create the player's character if the character doesn't exist
     # global name
@@ -29,16 +68,17 @@ def charcre():
             os.mkdir('../User_Files/'+name)
 
             charfile = open('../User_Files/'+name+'/'+name, 'w+')
-            eldersup = open('../User_Files/'+name+'/eldersup', 'w+')
-            eldersup.write('0')
-            arrival = open('../User_Files/'+name+'/arrival', 'w+')
-            arrival.write('0')
-            villagestatus = open('../User_Files/'+name+'/villagestatus', 'w+')
-            villagestatus.write('1')
+            # eldersup = open('../User_Files/'+name+'/eldersup', 'w+')
+            # eldersup.write('0')
+            # arrival = open('../User_Files/'+name+'/arrival', 'w+')
+            # arrival.write('0')
+            # villagestatus = open('../User_Files/'+name+'/villagestatus', 'w+')
+            # villagestatus.write('1')
             print('')
             while True:
                 corey = input('Would you like this character to be Hardcore? You would lose your save upon death. YES/NO: ')
                 if corey == 'YES':
+                    print('Don\'t make the wrong choice!')
                     charfile.write('../User_Files/'+name+' \n0\nAlive\nHardcore')
                     charfile.close()
                     break
@@ -59,3 +99,10 @@ def inventory_check():
 def game_help():
     #A function to show the list of actions the player can perform
     pass
+
+#def start_village()
+
+
+#intro() #Introducing instructions, plays every time the game is started
+#namecheck() #Asks the user's name, if name doesn't exist, calls upon the charcre function to create it
+original_intro()
